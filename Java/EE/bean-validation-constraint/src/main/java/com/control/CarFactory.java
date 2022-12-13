b881ab78df43056e1aa99fb2a02b7b0e9a17d221
amirsnw <59132773+amirsnw@users.noreply.github.com>
2022-12-13 17:49:43 +0330

    bean validation

--- Java/EE/bean-validation-constraint/src/main/java/com/control/CarFactory.java
+++ Java/EE/bean-validation-constraint/src/main/java/com/control/CarFactory.java
@@ -9,8 +9,6 @@ import java.util.UUID;
 public class CarFactory {
 
     public Car createCar(Specification specification) {
-        if (new Random().nextBoolean())
-            throw new CarCreationException("could not create car");
         Car car = new Car();
         car.setIdentifier(UUID.randomUUID().toString());
         car.setColor(specification.getColor());

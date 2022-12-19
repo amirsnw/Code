8a4e9d931bca7467886c6b875e4ded37c9c7de7e
amirsnw <59132773+amirsnw@users.noreply.github.com>
2022-12-19 17:10:51 +0330

    protected and default package

--- Java/SE/OCA-OCP/src/edu/matkosoric/inheritance/accessors/commonwealth/Australia.java
+++ Java/SE/OCA-OCP/src/edu/matkosoric/inheritance/accessors/commonwealth/Australia.java
@@ -6,6 +6,8 @@ package edu.matkosoric.inheritance.accessors.commonwealth;
  * Created by © Matko Soric.
  */
 
+import edu.matkosoric.inheritance.accessors.commonwealth.overseas.territories.Bermuda;
+
 public class Australia extends UnitedKingdom {
 
     // Australia class is a subclass of the UnitedKingdom class,
@@ -15,11 +17,23 @@ public class Australia extends UnitedKingdom {
     public static void main(String[] args) {
 
         Australia au = new Australia();
+        UnitedKingdom ukAus = new Australia();
+        UnitedKingdom uk = new UnitedKingdom();
+
         System.out.println(au.citizenship);
         System.out.println(au.currency);
         System.out.println(au.queen);
 //        System.out.println(au.capitalCity);     // does not compile, private access
 
+        System.out.println(ukAus.citizenship);
+        System.out.println(ukAus.currency);
+        System.out.println(ukAus.queen);
+//        System.out.println(ukAus.capitalCity);
+
+        System.out.println(uk.citizenship);
+        System.out.println(uk.currency);
+        System.out.println(uk.queen);
+//        System.out.println(uk.capitalCity);
     }
 
 }
--- Java/SE/OCA-OCP/src/edu/matkosoric/inheritance/accessors/commonwealth/overseas/territories/Bermuda.java
+++ Java/SE/OCA-OCP/src/edu/matkosoric/inheritance/accessors/commonwealth/overseas/territories/Bermuda.java
@@ -18,12 +18,23 @@ public class Bermuda extends UnitedKingdom {
     public static void main(String[] args) {
 
         Bermuda bermuda = new Bermuda();
+        UnitedKingdom ukBer = new Bermuda();
+        UnitedKingdom uk = new UnitedKingdom();
 
         System.out.println(bermuda.citizenship);     // public access in UK class
         System.out.println(bermuda.currency);        // protected access in the UK class
 //        System.out.println(bermuda.queen);           // does not compile, default access in the UK class
 //        System.out.println(bermuda.capitalCity);     // does not compile, private access in the UK class
 
+        System.out.println(ukBer.citizenship);
+//        System.out.println(ukBer.currency);
+//        System.out.println(ukBer.queen);
+//        System.out.println(ukBer.capitalCity);
+
+        System.out.println(uk.citizenship);
+//        System.out.println(uk.currency);
+//        System.out.println(uk.queen);
+//        System.out.println(uk.capitalCity);
     }
 
 }

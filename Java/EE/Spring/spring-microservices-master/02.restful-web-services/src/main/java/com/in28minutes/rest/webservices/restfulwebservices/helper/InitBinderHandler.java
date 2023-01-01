02204380495de2ba7f87e01d497da188f3cc8496
amirsnw <59132773+amirsnw@users.noreply.github.com>
2023-01-01 09:07:05 +0330

    repackage initBinder

--- Java/EE/Spring/spring-microservices-master/02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helper/InitBinderHandler.java
+++ Java/EE/Spring/spring-microservices-master/02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helper/InitBinderHandler.java
@@ -1,4 +1,4 @@
-package com.in28minutes.rest.webservices.restfulwebservices.exception;
+package com.in28minutes.rest.webservices.restfulwebservices.helper;
 
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.web.bind.WebDataBinder;
